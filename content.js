f8f242979fa24ef01049d18b70e619cc49c7d803
Hangs Breaker <hangsbreaker@gmail.com>
2023-11-03 15:16:49 +0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,12 @@
+let url = window.location.href;
+if (url.includes("youtube.com")) {
+  setInterval(() => {
+    var skip = document.getElementsByClassName("ytp-ad-skip-button");
+    if (skip[0] != null) {
+      // console.log("exists");
+      skip[0].click();
+    } else {
+      // console.log("not found");
+    }
+  }, 1000);
+}

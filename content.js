51cd3c6efb66bffaae0cc06d254de0961b3f56f6
Hangs Breaker <hangsbreaker@gmail.com>
2023-11-15 16:59:11 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 let url = window.location.href;
 if (url.includes("youtube.com")) {
   setInterval(() => {
-    var skip = document.getElementsByClassName("ytp-ad-skip-button");
+    var skip = document.getElementsByClassName("ytp-ad-skip-button-modern");
     if (skip[0] != null) {
       // console.log("exists");
       skip[0].click();
